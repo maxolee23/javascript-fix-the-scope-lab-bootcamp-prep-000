b8fc8a5dc1754e10d50ce17bfd0824dff5a7df2a
Max Lee <maxolee23@gmail.com>
2019-06-05 17:20:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,12 +7,11 @@ function yourAnimal() {
   animal = 'cat'
   return animal
 }
-
+const two = 2
 function add2(n) {
   return n + two
 
   // Feel free to move things around!
-  const two = 2
 }
 
 var funkyFunction = function() {
